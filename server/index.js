0c6021cb23fb6659e870efaf9ddec5af625342e7
divalsehgal9 <dival.sehgal@nineleaps.com>
2020-06-25 14:48:58 +0530

    changes in index files

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const io = require("socket.io")(server);
 const config = require("./config/key");
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true})
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
